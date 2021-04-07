ba0ef3c8b4903f117a65e8384642c754c4023a0a
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2021-04-06 23:47:56 -0700

    Add descriptive comments

--- main.go
+++ main.go
@@ -27,11 +27,15 @@ func main() {
 	uri := os.Getenv("MONGO_URI")
 	clientOpts := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(ctx, clientOpts)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Disconnect(ctx)
 	collection := client.Database("nft").Collection("opensea_events")
 
-	for i:=0; i<200; i++ {
+	// OpenSea events API capped at 200 pages at a time
+	// Rerunning the task manually appears to be enough delay to reset counter
+	for i := 0; i <= 200; i++ {
 		events := fetchEvents(i)
 
 		if events == nil {
@@ -39,7 +43,9 @@ func main() {
 			break
 		}
 		_, err := collection.InsertMany(ctx, events)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Print(".")
 	}
 	getLatestRecord(collection)
@@ -47,18 +53,23 @@ func main() {
 	fmt.Println("Program complete")
 }
 
-type Events struct {
+// Don't worry about event structure, we just know it's an array of json objects
+type OpenSeaEvents struct {
 	Events []interface{} `json:"asset_events"`
 }
 
+// Fetch events from the OpenSea API, returning as an array of JSON objects
+// Page of API results specified as parameter; starting timestamp is hard-coded
+// based on earliest timestamp of previous batch.
+// Getting this tinestamp and manually copy-pasting into this URI is a tedious hack
 func fetchEvents(page int) []interface{} {
-	url := fmt.Sprintf("https://api.opensea.io/api/v1/events?only_opensea=false&offset=%d&limit=50&occurred_before=1616107702&event_type=successful", page * 50)
+	url := fmt.Sprintf("https://api.opensea.io/api/v1/events?only_opensea=false&offset=%d&limit=50&occurred_before=1615829799&event_type=successful", page*50)
 	response, err := http.Get(url)
 	if err != nil {
 		log.Printf("Request Failed: %s", err)
 		return nil
 	}
-	var events Events
+	var events OpenSeaEvents
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
@@ -70,13 +81,17 @@ func fetchEvents(page int) []interface{} {
 	return events.Events
 }
 
+// Interface for OpenSea event data
+// Only care about date so ignore all other fields
 type EventDate struct {
 	CreatedDate string `bson:"created_date"`
 }
 
+// Print earliest timestamp in Collection
+// Note this means you can't change event_type in the OpenSea request between runs, or you'll get different data
 func getLatestRecord(collection *mongo.Collection) {
 
-	opts := options.FindOne().SetSort(bson.D{{"created_date",1}})
+	opts := options.FindOne().SetSort(bson.D{{"created_date", 1}})
 	var createdDate EventDate
 	err := collection.FindOne(nil, bson.D{}, opts).Decode(&createdDate)
 	if err != nil {
